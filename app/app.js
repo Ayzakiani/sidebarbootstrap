ed2ad08af78cf59e01d65142454f056330268600
ayza kiani <ayzakiani@hotmail.com>
2015-09-08 17:34:23 +0500

    submit check on form

--- app/app.js
+++ app/app.js
@@ -1,40 +1,32 @@
-  var mylinkApp = angular.module('mylinkApp', ['ui.router']);
+    (function(){
+      angular.module('mylinkApp', ['ui.router'])
 
+      .config(['$stateProvider', '$urlRouterProvider',
 
-  mylinkApp.config(['$stateProvider', '$urlRouterProvider',
+      function($stateProvider, $urlRouterProvider) {
 
-  function($stateProvider, $urlRouterProvider) {
-
-    $urlRouterProvider.otherwise('/index.html');
-    
-    $stateProvider.state('/home', {
-      url: "/home",
-      templateUrl: "views/home.html",
- 
-    })
-      .state('/about', {
-      url: "/about",
-      templateUrl: "views/about.html",
-      controller: 'aboutController'
-    })
-      .state('/siteMap', {
-      url: "/siteMap",
-      templateUrl: "views/siteMap.html",
-    
-    })
-
-  }]);
-
-  // mylinkApp.controller('mainController', function($scope, $state) {
+        // $urlRouterProvider.otherwise('/index.html');
+        
+        $stateProvider.state('home', {
+          url: '/home',
+          templateUrl: 'views/home.html',
      
- 
-  // });
+        })
+          .state('/about', {
+          url: "/about",
+          templateUrl: "views/about.html",
+         
+        })
+          .state('/siteMap', {
+          url: "/siteMap",
+          templateUrl: "views/siteMap.html",
+        
+        })
 
-  mylinkApp.controller('aboutController', ['$scope', 'userService', function($scope, userService) {
-    $scope.userService = userService;
-    $scope.user = $scope.userService.user;
-    $scope.message = 'No data to show form not submitted!.';
-  }]);
+
+      }]);
 
   
+
+ })();
  
