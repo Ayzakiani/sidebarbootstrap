8092bf33f652fd2d1727bf2a9c0ba255bf876b1c
Hassan Rehman <harehman@cisco.com>
2015-10-08 02:40:45 -0700

    indendation changes

--- app/js/mlSitemapDirective.js
+++ app/js/mlSitemapDirective.js
@@ -23,7 +23,8 @@
               $scope.gameFieldArray[i][j] = false;
               }
             }
-          }
+          };
+
           $scope.startnewGame = function (num) {
           	$scope.hideForm = true;
           	$scope.buttonTwo = true;
@@ -33,28 +34,30 @@
            	}  
           	$scope.message = '' ;      
           	create2dArray(num);
-          }
+          };
+
           $scope.startGame = function (){
           	$scope.hideForm = false;
           	$scope.buttonTwo = false;
           	$scope.tableHide = true;
-          }
+          };
+
           $scope.oncheck = function (row, cell) {
-          	console.log($scope.isClickEnable);
           	if($scope.gameFieldArray[row][cell] === false){ 
-          	$scope.gameFieldArray[row][cell] = currentPlayer;
-						checkforWinner();
+          	  $scope.gameFieldArray[row][cell] = currentPlayer;
+						  checkforWinner();
 	            if (playerOne) {
-	            currentPlayer = 'O';
-	            playerOne = false;
-	            playerTwo = true;
+	              currentPlayer = 'O';
+	              playerOne = false;
+	              playerTwo = true;
 	            } else {
-	            currentPlayer = 'X';
-	            playerOne = true;
-	            playerTwo = false;
-	              }    	                                 
+	              currentPlayer = 'X';
+	              playerOne = true;
+	              playerTwo = false;
+	            }    	                                 
 					  }
-          }      
+          };
+
           var checkforWinner = function(){
          		for (var i = 0; i < $scope.rowLength; i++ ){ 
           	var counter = 0; 
@@ -64,56 +67,56 @@
 			        	if(currentPlayer){
 			        		//Checking Winner In All Rows.
 			          	if($scope.gameFieldArray[i][j] == currentPlayer){
-			          	counter++;
+			          	  counter++;
 			         			if (counter === $scope.streakLength){
-			          		$scope.message = currentPlayer + ' Row wins!';
-						        $scope.isClickEnable = false;
-						        return true;
-
+			          		  $scope.message = currentPlayer + ' Row wins!';
+						          $scope.isClickEnable = false;
+						          return true;
 			          		} 	
 			          	} else {
-			          	counter = 0;
+			          	  counter = 0;
 			          	}
 			          	//Checking Winner In All Columns.
 			            if($scope.gameFieldArray[j][i] == currentPlayer){
-          		    counterOne++;
+          		      counterOne++;
           		 			if (counterOne === $scope.streakLength){
-          		 			$scope.message = currentPlayer + ' Colunm wins!';
-          		 			$scope.isClickEnable = false;
-          		 			return true;
+          		 			  $scope.message = currentPlayer + ' Colunm wins!';
+          		 			  $scope.isClickEnable = false;
+          		 			  return true;
           		 	  	}
           		 		} else {
-          		 		counterOne = 0;
+          		 		  counterOne = 0;
           		 	 	} 
           		 	 	//Checking Winner In Left Diagonal.
           		 		if($scope.gameFieldArray[j][j] == currentPlayer){
-          		 		counterTwo++;
+          		 		  counterTwo++;
           		 			if (counterTwo === $scope.streakLength){
-          		 		  $scope.message = currentPlayer + ' Left Diagonal wins!';
-          		 		  $scope.isClickEnable = false;
-          		 		 	return true;
+          		 		    $scope.message = currentPlayer + ' Left Diagonal wins!';
+          		 		    $scope.isClickEnable = false;
+          		 		 	  return true;
           		 		 	}
           		 		} else {
-          		 		counterTwo = 0;
+          		 		  counterTwo = 0;
           		 	 	}
           		 	 	//Checking Winner In Right Diagonal.
-          		 	checkDiagonal2();
+          		 	  checkDiagonal2();
 			          }
 			        }
 			      }
 			      return false;
-			    }	    	 		 
+			    };
+
 	       	var checkDiagonal2 = function (){
 	       	var row = 0;
 	       	var counterthree = 0;
 	       	 	for(var a = $scope.rowLength - 1; a >= 0; a--){
 							if(currentPlayer) {
 								if($scope.gameFieldArray[row][a] == currentPlayer) {
-								counterthree++;
+								  counterthree++;
 									if (counterthree === $scope.streakLength) {
-									$scope.message = currentPlayer + ' Right Diagonal wins!';
-									$scope.isClickEnable = false;
-									return true;
+									  $scope.message = currentPlayer + ' Right Diagonal wins!';
+									  $scope.isClickEnable = false;
+									  return true;
 									}
 								}
 							}	
@@ -125,4 +128,3 @@
       }
     });
 })();
-
